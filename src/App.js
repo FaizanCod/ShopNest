7699e08d8036090d715e46170c746195cded8322
Faizan Choudhary <faizan2000461@st.jmi.ac.in>
2023-07-18 23:58:02 +0530

    login, signup pages

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import Store from "./pages/Store";
 import Blog from "./pages/Blog";
 import Compare from "./pages/Compare";
 import Wishlist from "./pages/Wishlist";
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
+import Signup from "./pages/Signup";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
 	return (
@@ -24,6 +28,10 @@ function App() {
 						<Route path="blogs" element={<Blog />} />
 						<Route path="compare-products" element={<Compare />} />
 						<Route path="wishlist" element={<Wishlist />} />
+						<Route path="login" element={<Login />} />
+						<Route path="forgot-password" element={<ForgotPassword />} />
+						<Route path="signup" element={<Signup />} />
+						<Route path="reset-password" element={<ResetPassword />} />
 					</Route>
 				</Routes>
 			</BrowserRouter>
